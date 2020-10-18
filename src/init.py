5056475817c6078531eb4a107cc8568f58a30efd
soheil <ssoheil@chalmers.se>
2020-10-18 11:23:58 +0200

    Correct PEP 8 warnings

--- src/init.py
+++ src/init.py
@@ -1,17 +1,14 @@
-from feature import extract
 from skimage import io, data
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import img_as_float as imfloat
-from skimage import filters
 
 
-def examine( imagePath, save=False, verbose=False, graphics=False ):
-    image = imfloat( io.imread( imagePath ) )
+def examine(imagePath, save=False, verbose=False, graphics=False):
+    image = imfloat(io.imread(imagePath))
 
     if verbose:
-        np.set_printoptions( precision = 2,
-                             linewidth = 155 )
+        np.set_printoptions(precision=2, linewidth=155)
         
         print('Image type: ', type(image))
         print('Image datatype: ', image.dtype)
@@ -19,7 +16,7 @@ def examine( imagePath, save=False, verbose=False, graphics=False ):
 
     if graphics:
         gridShow = False
-        fig, ax1 = plt.subplots(1,1)
+        fig, ax1 = plt.subplots(1, 1)
 
         # TODO: can we expand imagePath and loop over if it has more than 1 path?
         ax1.imshow(image, cmap='gray')
@@ -29,7 +26,7 @@ def examine( imagePath, save=False, verbose=False, graphics=False ):
 
     # save the image
     if save:
-        np.save('./data/input_image', image)
+        np.save('../data/input_image', image)   # TODO: Find the installation path; then cd to data/
 
     # return the x, and y ranges
     x_max = image.shape[1]
@@ -37,26 +34,17 @@ def examine( imagePath, save=False, verbose=False, graphics=False ):
 
     return image, x_max, y_max
 
+
 if __name__ == '__main__':
     image, x_range, y_range = examine('./tests/test.jpg', save=False, verbose=True, graphics=False)
     print(f'Image has x-range = {x_range}, and y-range = {y_range}')
     
-    imgGray  = image @ [0.2126, 0.7152, 0.0722]  # image in grayscale
+    imgGray = image @ [0.2126, 0.7152, 0.0722]  # image in grayscale
 
     gridShow = False
-    fig, ax1 = plt.subplots(1,1)
+    fig, ax1 = plt.subplots(1, 1)
 
     ax1.imshow(imgGray, cmap='gray')
     ax1.set_title('Test image', fontsize=14)
     ax1.grid(gridShow)
     plt.show()
-        
-    
- 
-    
-
-
-
-
-
-
