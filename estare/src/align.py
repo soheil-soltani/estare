cc2a3c317653b2e07b0c520506485ce2f8f158fe
Soheil <soheil@netc.eu>
2021-01-31 12:37:40 +0100

    Correct the implementation of alignment algorithm

--- estare/src/align.py
+++ estare/src/align.py
@@ -3,158 +3,88 @@ from skimage import img_as_float as imfloat
 import numpy as np
 from matplotlib import pyplot as plt
 
+from src.init import examine
+from src.rotate import rotate
+
+
+# Fine-tuning
+# - pick features from scattered regions
+# - characterize error: does it vary if one goes towards the edges or far from the origin?
+# - compare light curves and make adjustments if the stacked feature deviates too much
 
 def align(image_1, image_2, pivot_1, pivot_2):
   
-    img_1 = imfloat(io.imread(image_1))  # 'tests/IMG_1740.JPG'
-    img_2 = imfloat(io.imread(image_2))  # 'tests/IMG_1741.JPG'
+    img_1, x_range_1, y_range_1 = examine(image_1)  
+    img_2, x_range_2, y_range_2 = examine(image_2)
 
-    coord_1 = np.load(pivot_1)  # 'data_new_layer_1/feature_1.npy'
-    coord_2 = np.load(pivot_2)  # 'data_new_layer_2/feature_1.npy'
+    # TODO: How to raise error here?
+    # if (x_range_1 != x_range_2) or (y_range_1 != y_range_2):
+    #     #TODO: raise error and exit
+    #     pass   # JUST FOR NOW!
+    
+    # two reference points for alignment
+    coord_1 = np.load(pivot_1)
+    a_1 = coord_1[0]
+    b_1 = coord_1[1]
+    
+    # two target points that should match the reference points after alignment
+    coord_2 = np.load(pivot_2)  
+    a_2 = coord_2[0]
+    b_2 = coord_2[1]
 
-    print(f'x1 = {coord_1[1]}; y1 = {coord_1[0]}')
-    print(f'x2 = {coord_2[1]}; y2 = {coord_2[0]}')
+    # Use the two points to back calculate the angle of rotation
+    slope_bef = ( b_1[1] - a_1[1] )/( b_1[0] - a_1[0] )
+    
+    slope_aft = ( b_2[1] - a_2[1] )/( b_2[0] - a_2[0] )
 
-    # TODO: x_range and y_range should be inferred
-    x_range = 2256
-    y_range = 1504
+    theta = ( ((np.arctan(slope_aft))*180/np.pi) -
+              ( (np.arctan(slope_bef))*180/np.pi ) )
 
-    # TODO: number of layers (3) must be inferred
-    stacked = np.zeros((y_range, x_range, 3), dtype=float)
-    del_x = coord_2[1] - coord_1[1] 
-    del_y = coord_2[0] - coord_1[0] 
+    a2_derot = rotate(a_2, -theta, discrete=False)   # set discrete=False to get an exact value
 
-    for i in range(x_range):
-        for j in range(y_range):
-            stacked[j, i, 0] = img_1[j, i, 0]
-            stacked[j, i, 1] = img_1[j, i, 1]
-            stacked[j, i, 2] = img_1[j, i, 2]
+    # compare the derotated one from its origin to calculate the relative translation magnitude 
+    dxp = a2_derot[0] - a_1[0]
+    dyp = a2_derot[1] - a_1[1]
 
-    for i in range(y_range-abs(del_y)):
-        for j in range(abs(del_x), x_range):            
-            stacked[i, j, 0] += img_2[i+del_y, j+del_x, 0] 
-            stacked[i, j, 1] += img_2[i+del_y, j+del_x, 1] 
-            stacked[i, j, 2] += img_2[i+del_y, j+del_x, 2] 
+    # Using the formulae, compute the absolute translation magnitude
+    theta_rad = -theta * np.pi / 180   # radians
 
-    fig, ((frame, signal), (pile, dummy)) = plt.subplots(2, 2)
-    frame.imshow(img_1)
-    frame.set_title('Original')
+    del_y = ( dyp - dxp*np.tan(theta_rad) ) / \
+            (np.sin(theta_rad)*np.tan(theta_rad) + np.cos(theta_rad))
 
-    signal.plot(img_1[coord_1[0], coord_1[1]-10:coord_1[1]+10, 0], 'b', label='original')
+    del_x = (dxp + del_y*np.sin(theta_rad)) / np.cos(theta_rad)
+    
+    # Allocate x- and y-array for holding the new coordinates after offseting
+    x_array = np.zeros((x_range_1, y_range_1, 1), dtype=int)
+    y_array = np.zeros((x_range_1, y_range_1, 1), dtype=int)
+
+
+    # For the two following time-consuming loops, we need unit tests to ensure the correctness after each optimization:
+    # Now we can detranslate all points
+    for i in range(x_range_1):
+        for j in range(y_range_1):
+            x_array[i, j] = i-del_x
+            y_array[i, j] = j-del_y
+    
+    # and now we can derotate them 
+    for i in range(x_range_1):
+        for j in range(y_range_1):
+            uncorrected_coor = [x_array[i, j], y_array[i, j]]
+            corrected_coor = rotate(uncorrected_coor, -theta, discrete=True)
             
-    signal.plot(stacked[coord_1[0], coord_1[1]-10:coord_1[1]+10, 0]/2.0, 'k', label='del_x fr. feat.')
+            x_array[i, j] = corrected_coor[0]
+            y_array[i, j] = corrected_coor[1]
+
+            # stack
+            if x_array[i, j] > 0 and x_array[i, j] < x_range_1 and y_array[i, j] > 0 and y_array[i, j] < y_range_1:
+                img_1[x_array[i, j], y_array[i, j]] += img_2[i, j]   # Broadcasting to all three channels is implicit
+
+    #io.imsave('/home/minter/workdir/Central_backup/Pictures/Test_images_for_estare_Ramberget_Dec_2020/estare_stacked.JPG', img_1)
+
+    # # recons = imread('/home/minter/workdir/Central_backup/Pictures/Test_images_for_estare_Ramberget_Dec_2020/DSC00281_aligned.JPG')
+
+
+
+
+
     
-    signal.grid('True')
-    signal.legend()
-
-    pile.imshow(stacked)
-    pile.set_title('Stacked')
-    
-    plt.show()    
-
-    # TODO: Clip the final image to remove the edge bands
-
-    
-def mismatch(parameters):
-    m, n = parameters
-    img_1 = imfloat(io.imread('tests/IMG_1735.JPG'))
-    img_2 = imfloat(io.imread('tests/IMG_1736.JPG'))
-
-    coord_1 = np.load('data_layer1/feature_1.npy')
-    coord_2 = np.load('data_layer2/feature_1.npy')
-
-    x_range = 2256
-    y_range = 1504
-
-    stacked = np.zeros((y_range, x_range, 3), dtype=float)
-    del_x = (coord_2[1] - coord_1[1]) + int(round(m))
-    del_y = (coord_2[0] - coord_1[0]) + int(round(n))
-
-    for i in range(y_range-abs(del_y)):
-        for j in range(abs(del_x), x_range):
-            stacked[i - del_y, j - del_x, 0] = img_2[i, j, 0] + img_1[i - del_y, j - del_x, 0]
-            stacked[i - del_y, j - del_x, 1] = img_2[i, j, 1] + img_1[i - del_y, j - del_x, 1]
-            stacked[i - del_y, j - del_x, 2] = img_2[i, j, 2] + img_1[i - del_y, j - del_x, 2]
-
-    errorRed = stacked[:, :, 0] - img_1[:, :, 0]
-    errorGreen = stacked[:, :, 1] - img_1[:, :, 1]
-    errorBlue = stacked[:, :, 2] - img_1[:, :, 2]
-
-    error = (np.sum(errorRed) + np.sum(errorGreen) + np.sum(errorBlue))/np.sum(img_1[...])
-
-    return error
-
-
-def align_archived():
-    # img_1 = imfloat(io.imread('tests/IMG_1735.JPG'))
-    # img_2 = imfloat(io.imread('tests/IMG_1736.JPG'))
-
-    img_1 = imfloat(io.imread('tests/IMG_1740.JPG'))
-    img_2 = imfloat(io.imread('tests/IMG_1741.JPG'))
-
-    coord_1 = np.load('data_new_layer_1/feature_1.npy')
-    coord_2 = np.load('data_new_layer_2/feature_1.npy')
-
-    print(f'x1 = {coord_1[1]}; y1 = {coord_1[0]}')
-    print(f'x2 = {coord_2[1]}; y2 = {coord_2[0]}')
-
-    x_range = 2256
-    y_range = 1504
-
-    stacked = np.zeros((y_range, x_range, 3), dtype=float)
-    del_x = 0   # coord_2[1] - coord_1[1] #- 2
-    del_y = 0   # coord_2[0] - coord_1[0] #+ 3
-
-    # for i in range(abs(del_x), x_range):
-    #     for j in range(abs(del_y), y_range):
-    #         stacked[j-del_y, i-del_x] = 1.0 #img_2[j, i, 0]
-
-    # x0 = np.array([1, 1])
-    # res = argmin.minimize(mismatch, x0, method='nelder-mead', bounds=([-5, 5], [-5, 5]),
-    #                       options={'xatol' : 1e-3, 'disp' : True})
-    # print(res.x)
-
-    # del_x += int(round(res.x[0])) 
-    # del_y += int(round(res.x[1]))
-    
-    # for i in range(y_range-abs(del_y)):
-    #     for j in range(abs(del_x), x_range):
-    for i in range(abs(del_y), y_range):
-        for j in range(x_range-abs(del_x)):
-            stacked[i - del_y, j - del_x, 0] = img_2[i, j, 0] + img_1[i - del_y, j - del_x, 0]
-            stacked[i - del_y, j - del_x, 1] = img_2[i, j, 1] + img_1[i - del_y, j - del_x, 1]
-            stacked[i - del_y, j - del_x, 2] = img_2[i, j, 2] + img_1[i - del_y, j - del_x, 2]
-
-    # fig, (top, mid, bot) = plt.subplots(3, 1)
-    # top.imshow(img_1)
-    # mid.imshow(img_2)
-    # bot.imshow(stacked)
-    fig, (frame, signal) = plt.subplots(1, 2)
-    frame.imshow(stacked)
-    frame.set_title('Original')
-    signal.plot(img_1[coord_1[0], coord_1[1]-10:coord_1[1]+10, 0], 'b', label='original')
-
-    del_x = 1
-    del_y = 0
-
-    for i in range(abs(del_y), y_range):
-        for j in range(abs(del_x), x_range):
-            stacked[i - del_y, j - del_x, 0] = img_2[i, j, 0] + img_1[i - del_y, j - del_x, 0]
-            stacked[i - del_y, j - del_x, 1] = img_2[i, j, 1] + img_1[i - del_y, j - del_x, 1]
-            stacked[i - del_y, j - del_x, 2] = img_2[i, j, 2] + img_1[i - del_y, j - del_x, 2]
-
-    signal.plot(stacked[coord_1[0], coord_1[1]-10:coord_1[1]+10, 0]/2.0, 'r', label='del_x=1')
-
-    del_x = coord_2[1] - coord_1[1] 
-    del_y = 0   # coord_2[0] - coord_1[0]
-
-    for i in range(abs(del_y), y_range):
-        for j in range(x_range-abs(del_x)):
-            stacked[i - del_y, j - del_x, 0] = img_2[i, j, 0] + img_1[i - del_y, j - del_x, 0]
-            stacked[i - del_y, j - del_x, 1] = img_2[i, j, 1] + img_1[i - del_y, j - del_x, 1]
-            stacked[i - del_y, j - del_x, 2] = img_2[i, j, 2] + img_1[i - del_y, j - del_x, 2]
-
-    signal.plot(stacked[coord_1[0], coord_1[1]-10:coord_1[1]+10, 0]/2.0, 'k', label='del_x fr. feat.')
-    signal.grid('True')
-    signal.legend()
-    plt.show()
