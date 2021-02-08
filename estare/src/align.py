beaa5cdbc79098552c295eecbdf268bd910ea81f
Soheil <soheil@netc.eu>
2021-02-08 21:29:01 +0100

    Implement derotate() function.

--- estare/src/align.py
+++ estare/src/align.py
@@ -3,8 +3,8 @@ from skimage import img_as_float as imfloat
 import numpy as np
 from matplotlib import pyplot as plt
 
-from src.init import examine
-from src.rotate import rotate
+from estare.src.init import examine
+from estare.src.rotate import rotate
 
 # +++++++++++++
 # For profiling
@@ -17,38 +17,49 @@ import time
 # - compare light curves and make adjustments if the stacked feature deviates too much
 
 
-@profile
 def detrans(x_array, y_array, x_range, y_range, del_x, del_y):
 
-    '''
-Corrects the effect of translation by removing the extent by which each point 
-had been shifted, i.e. del_x, and del_y.
-'''
-    # for i in range(x_range):
-    #     for j in range(y_range):
-    #         x_array[i, j] = i-del_x
-    #         y_array[i, j] = j-del_y
+    """Corrects the effect of translation by removing the extent by which each point 
+    had been shifted, i.e. del_x, and del_y.
 
+    Args:
+    x_array
+    y_array
+    x_range
+    y_range
+    del_x
+    del_y
+
+    Returns:
+    x_array
+    y_array
+
+    """
+    
     for i in range(x_range):
         x_array[i, :] = i-del_x
 
     for j in range(y_range):
         y_array[:, j] = j-del_y
-            
-
+   
+    
     return x_array, y_array
 
-#@profile
-def align(image_1, image_2, pivot_1, pivot_2):
-  
-    img_1, x_range_1, y_range_1 = examine(image_1)  
-    img_2, x_range_2, y_range_2 = examine(image_2)
 
-    # TODO: How to raise error here?
-    # if (x_range_1 != x_range_2) or (y_range_1 != y_range_2):
-    #     #TODO: raise error and exit
-    #     pass   # JUST FOR NOW!
+
+def detrans_scalar(x_array, y_array, x_range, y_range, del_x, del_y):
     
+    for i in range(x_range):
+        for j in range(y_range):
+            x_array[i, j] = i-del_x
+            y_array[i, j] = j-del_y
+            
+    
+    return x_array, y_array
+
+
+
+def find_offset(pivot_1, pivot_2):
     # two reference points for alignment
     coord_1 = np.load(pivot_1)
     a_1 = coord_1[0]
@@ -80,11 +91,48 @@ def align(image_1, image_2, pivot_1, pivot_2):
             (np.sin(theta_rad)*np.tan(theta_rad) + np.cos(theta_rad))
 
     del_x = (dxp + del_y*np.sin(theta_rad)) / np.cos(theta_rad)
+
+
+    return del_x, del_y, theta, theta_rad
+
+
+
+def derotate(x_array, y_array, theta, roundup=False):
+    """Removes the effect of rotation
+    """
+
+    x_range_1 = np.shape(x_array)[0]
+    y_range_1 = np.shape(x_array)[1]
+
+    print(x_range_1, y_range_1)
     
+    for i in range(x_range_1):
+        for j in range(y_range_1):
+            uncorrected_coor = [x_array[i, j], y_array[i, j]]
+            corrected_coor = rotate(uncorrected_coor, -theta, discrete=roundup)
+            
+            x_array[i, j] = corrected_coor[0]
+            y_array[i, j] = corrected_coor[1]
+
+    return x_array, y_array
+
+                
+def align(image_1, image_2, pivot_1, pivot_2):
+  
+    img_1, x_range_1, y_range_1 = examine(image_1)  
+    img_2, x_range_2, y_range_2 = examine(image_2)
+
+ 
+    # if (x_range_1 != x_range_2) or (y_range_1 != y_range_2):
+    #     #TODO: raise error and exit
+
+
+    # calculate offsets (del_x, del_y) and rotation angle theta
+    del_x, del_y, theta, theta_rad = find_offset(pivot_1, pivot_2)
+    print(del_x, del_y, theta, theta_rad)
     # Allocate x- and y-array for holding the new coordinates after offseting
     x_array = np.zeros((x_range_1, y_range_1, 1), dtype=int)
     y_array = np.zeros((x_range_1, y_range_1, 1), dtype=int)
-
 
     # For the two following time-consuming loops, we need unit tests to ensure the correctness after each optimization:
     # Now we can detranslate all points
@@ -111,9 +159,8 @@ def align(image_1, image_2, pivot_1, pivot_2):
     print('De-translation took %s sec.'%(t_2-t_1))
     print('Stacking took %s sec.'%(t_3-t_2))
     print('Done')
-    io.imsave('/home/minter/workdir/Central_backup/Pictures/Test_images_for_estare_Ramberget_Dec_2020/estare_stacked.JPG', img_1)
+    #io.imsave('/home/minter/workdir/Central_backup/Pictures/Test_images_for_estare_Ramberget_Dec_2020/estare_stacked.JPG', img_1)
 
-    # # recons = imread('/home/minter/workdir/Central_backup/Pictures/Test_images_for_estare_Ramberget_Dec_2020/DSC00281_aligned.JPG')
 
 
 
@@ -125,6 +172,7 @@ if __name__ == '__main__':
     y_array = np.zeros((x_range, y_range, 1), dtype=float)
 
     del_x = 0.1; del_y = 0.2
+
     
     t_1 = time.time()
     for t in range(10):
@@ -133,6 +181,15 @@ if __name__ == '__main__':
     t_2 = time.time()
 
     print('De-translation took %s sec.'%(t_2-t_1))
+
+    
+    t_1 = time.time()
+    for t in range(10):
+        x_array, y_array = detrans_scalar(x_array, y_array, x_range, y_range, del_x, del_y)
+            
+    t_2 = time.time()
+
+    print('De-translation (scal. version) took %s sec.'%(t_2-t_1))
 
 
     
