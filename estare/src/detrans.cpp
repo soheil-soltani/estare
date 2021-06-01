37f4741edb877cf97e5c799a7079efc060b800c0
Soheil <ssolt@mpcdf.mpg.de>
2021-06-01 20:46:01 +0200

    Add openmp

--- estare/src/detrans.cpp
+++ estare/src/detrans.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <omp.h>
 #include "boost/multi_array.hpp"
 
-// Compile with:  g++ -I /home/minter/Downloads/Software/Boost/boost_1_75_0 -o dtr detrans.cpp
+// Compile with:  g++ -I /home/minter/Downloads/Software/Boost/boost_1_75_0 -fopenmp -o dtr detrans.cpp
 typedef boost::multi_array <double, 2> array_2d;
 
 void detrans(array_2d &x_array,
@@ -27,24 +28,30 @@ void detrans(array_2d &x_array,
 
 int main() {
 
-  size_t x_range = 4;
-  size_t y_range = 3;
+  size_t x_range = 28000;   // 4;
+  size_t y_range = 28000;   // 3;
     
-  array_2d x_in(boost::extents[4][3]);
-  array_2d y_in(boost::extents[4][3]);
+  array_2d x_in(boost::extents[x_range][y_range]);
+  array_2d y_in(boost::extents[x_range][y_range]);
             
 
   double del_x = 0.1;
   double del_y = 0.2;
-  
+
+  double start = omp_get_wtime();
   detrans(x_in, y_in, x_range, y_range, del_x, del_y);
 
+  double stop = omp_get_wtime();
+  std::cout << "Detrans execution took " << stop-start << " sec.\n";
+  
+#ifdef DUMP_MATRIX  
   for (auto i = 0; i < x_range; ++i) {
     for (auto j = 0; j < y_range; ++j) {
       std::cout << x_in[i][j] << ' ';
     }
     std::cout << '\n';
   }
+#endif
   
   return 0;
 }
