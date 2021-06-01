ce3e4d79beb4a36e3b8a7da132aec47f008bf2b8
Soheil <ssolt@mpcdf.mpg.de>
2021-06-01 20:50:05 +0200

    Rename theta to deflection

--- estare/src/stack.py
+++ estare/src/stack.py
@@ -1,15 +1,23 @@
 
+import numpy as np
 from estare.src.rotate import rotate
 
 
-def stack(img_1, img_2, x_range, y_range, del_x, del_y, theta, discrete=True):
-
+def stack(img_1, img_2, x_range, y_range, del_x, del_y, deflection, discrete=True):
+    """ theta was changed to deflection
+    """
+    ###angle_matrix = np.load('../aux/angle_table.npy')
+    ###norm_matrix  = np.load('../aux/norm_matrix.npy')
+    
     for i in range(x_range):
         for j in range(y_range):
             #uncorrected_coor = [x_array[i, j], y_array[i, j]]
             uncorrected_coor = [i-del_x, j-del_y]
+
+            ###rho = norm_matrix[i, j]
+            ###theta = angle_matrix[i, j]
             
-            corrected_coor = rotate(uncorrected_coor, -theta, discrete=discrete)
+            corrected_coor = rotate(uncorrected_coor, -deflection, discrete=discrete)
             
             #x_array[i, j] = corrected_coor[0]
             #y_array[i, j] = corrected_coor[1]
