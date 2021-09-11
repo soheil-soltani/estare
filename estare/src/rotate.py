5d51192432c9d1970133fe1406348119e5185845
Soheil Soltani <soheil@netc.eu>
2021-09-11 11:04:28 +0200

    Vectorize arc() and rotate()
    In the vectorized version, the computations are done on the entire arrays of x- and y-coordinates.

--- estare/src/rotate.py
+++ estare/src/rotate.py
@@ -51,22 +51,62 @@ def arc(position, origo=np.array([0., 0.]), radians=False):
         return position_norm, np.arccos( (position[0]-origo[0]) / position_norm )
 
 
+def arc_vectorized(x_array, y_array, origo=np.array([0., 0.]), radians=False):
+    """Returns the angle between the input coordinate and the vertical 
+       axis Y using the projection principle
+    """
+
+    #TODO: position[0,0] or generally position=origin should raise error
+    # in fact division by rho = 0 should raise error
+    position_norm = np.sqrt((x_array-origo[0])**2 + (y_array-origo[1])**2)
+    # computing norm via np.linalg.norm(position) is much slower
+    if radians==False:
+        return position_norm, ( (180.0/np.pi) * (np.arccos( (x_array-origo[0]) / position_norm )) )
+    else:
+        return position_norm, np.arccos( (x_array-origo[0]) / position_norm )
+
     
 # apply rotation
 #@profile
-def rotate(position, deflection, origo=np.array([0., 0.]), radians=False, discrete=True):
-    """
-    #TODO: position [0,0] cannot be rotated
-    Change log:
+#def rotate(position, deflection, origo=np.array([0., 0.]), radians=False, discrete=True):
+#    """
+#    #TODO: position [0,0] cannot be rotated
+#    Change log:
 
-    28/03/2021
-    rho (which stands for 2-norm of the position vector) now comes directly from arc()
+#    28/03/2021
+#    rho (which stands for 2-norm of the position vector) now comes directly from arc()
     
-    02/04/2021
-    rho and deflection are pre-stored in a look-up table, which means that they just need to
-    be loaded
-    """
-    rho, theta = arc(position, radians=radians)
+#    02/04/2021
+#    rho and deflection are pre-stored in a look-up table, which means that they just need to
+#    be loaded
+
+#    11/09/2021 A vectorized version of arc() and rotate() is implemented
+#    """
+    # rho, theta = arc(position, radians=radians)
+    # theta += deflection
+    # #theta = deflection + arc(position, radians=radians)
+
+    # # Use the compiled version of arc():
+    # # theta = deflection + calculate.get_angle(position, radians=radians)
+
+    # if radians==False:
+    #     theta = theta*(np.pi/180.0)   
+
+    # # rho now comes directly from arc()
+    # #rho = np.sqrt((position[0]-origo[0])**2 + (position[1]-origo[1])**2)
+    
+    # x = rho*np.cos(theta)
+    # y = rho*np.sin(theta)
+
+    # if discrete:
+    #     rotated_position = np.rint([x, y])     
+    #     return np.array( [int(rotated_position[0]), int(rotated_position[1])] )
+    # else:
+    #     return np.array( [x, y] )
+
+    # Vectorized version
+def rotate(x_array, y_array, deflection, origo=np.array([0., 0.]), radians=False, discrete=True):
+    rho, theta = arc_vecorized(x_array, y_array, radians=radians)
     theta += deflection
     #theta = deflection + arc(position, radians=radians)
 
@@ -83,10 +123,11 @@ def rotate(position, deflection, origo=np.array([0., 0.]), radians=False, discre
     y = rho*np.sin(theta)
 
     if discrete:
-        rotated_position = np.rint([x, y])     
-        return np.array( [int(rotated_position[0]), int(rotated_position[1])] )
+        rotated_x = np.rint(x)
+        rotated_y = np.rint(y)     
+        return  int(rotated_x), int(rotated_y) 
     else:
-        return np.array( [x, y] )
+        return  x, y
 
 
 
